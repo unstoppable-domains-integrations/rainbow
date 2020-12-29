8db65639a7ad4b335791d442779d86dd2c406402
jinchung <jin.chung17@gmail.com>
2020-12-28 19:19:34 -0500

    Tidy up Compound references

--- src/references/index.ts
+++ src/references/index.ts
@@ -1,5 +1,5 @@
 import { mapKeys, mapValues } from 'lodash';
-import savingAssets from './compound/saving-assets.json';
+import { savingsAssets } from './compound';
 import { Asset } from '@rainbow-me/entities';
 
 export {
@@ -9,8 +9,7 @@ export {
   UNISWAP_V2_ROUTER_ADDRESS,
 } from './uniswap';
 export { default as chains } from './chains.json';
-export { default as compoundCERC20ABI } from './compound/compound-cerc20-abi.json';
-export { default as compoundCETHABI } from './compound/compound-ceth-abi.json';
+export { compoundCERC20ABI, compoundCETHABI } from './compound';
 export { default as emojis } from './emojis.json';
 export { default as erc20ABI } from './erc20-abi.json';
 export { default as ethUnits } from './ethereum-units.json';
@@ -77,10 +76,10 @@ export const DefaultUniswapFavorites = {
   ],
 };
 
-export const savingsAssetsList = savingAssets;
+export const savingsAssetsList = savingsAssets;
 
 export const savingsAssetsListByUnderlying = mapValues(
-  savingAssets,
+  savingsAssets,
   (assetsByNetwork: Record<string, Asset>) =>
     mapKeys(
       mapValues(assetsByNetwork, (assetByContract, contractAddress) => ({
